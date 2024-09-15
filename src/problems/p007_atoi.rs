49a7faeba80b93b85173228b7593511af0eb13ae
Hiromi ISHII <konn.jinro@gmail.com>
2024-09-15 14:14:53 +0900

    by_ref() and collect() once

--- src/problems/p007_atoi.rs
+++ src/problems/p007_atoi.rs
@@ -16,38 +16,39 @@ impl Solution {
             }
             _ => 1,
         };
-
-        let digits = iter
+        let mut digits = iter
             .skip_while(|v| *v == '0')
             .map_while(|c| c.to_digit(10))
-            .enumerate()
-            .try_fold(0, |acc, (idx, i)| {
-                if idx >= 10 {
-                    None
-                } else {
-                    Some(acc * 10 + i as i64)
-                }
-            });
-        match digits {
-            None => {
-                if sign == 1 {
-                    i32::MAX
-                } else {
-                    i32::MIN
-                }
+            // Without next two lines, !!! will match even if there are non-digit character other then digit!
+            .collect::<Vec<_>>()
+            .into_iter();
+        // If there are more than 10 digits, it clearly exceeds the 32-bit integer.
+        // So we only need to parse only first ten digits and return bounds if there are remaining digits.
+        let parsed = digits
+            .by_ref()
+            .take(10)
+            .fold(0, |acc, i| acc * 10 + i as i64);
+
+        if let Some(_) = digits.next() {
+            // !!! Should parse there are more consecutive digits.
+            if sign == 1 {
+                return i32::MAX;
+            } else {
+                return i32::MIN;
             }
-            Some(i) => {
-                let ub = i32::MAX as i64;
-                let lb = i32::MIN as i64;
-                let value = sign * i;
-                if value < lb {
-                    i32::MIN
-                } else if value > ub {
-                    i32::MAX
-                } else {
-                    value as i32
-                }
-            }
+        }
+
+        // Even if it has <=10 digits, it can still exceed the bounds.
+        // so we must check the bound.
+        let ub = i32::MAX as i64;
+        let lb = i32::MIN as i64;
+        let value = sign * parsed;
+        if value < lb {
+            i32::MIN
+        } else if value > ub {
+            i32::MAX
+        } else {
+            value as i32
         }
     }
 }
